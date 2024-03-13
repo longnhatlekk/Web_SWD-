e293f9185db42cfddf4848c47e1efa6d4e92d349
Nguyen Hong Ngoc <134280054+pby1905@users.noreply.github.com>
2024-03-13 17:31:23 +0700

    getallApi

--- src/pages/users/ListProduct/index.js
+++ src/pages/users/ListProduct/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
-import { productData } from '../../../components/ProductConstant';
+import axios from 'axios';
 import './ListProduct.css';
 
 const itemsPerPage = 5;
@@ -9,9 +9,23 @@ const itemsPerPage = 5;
 const ListProduct = () => {
     const navigate = useNavigate();
     const [currentPage, setCurrentPage] = useState(0);
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    // Use productData directly instead of useState
-    const products = productData;
+    useEffect(() => {
+        async function fetchProducts() {
+            try {
+                const response = await axios.get('https://localhost:7052/Products/ProductList');
+                setProducts(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+                setLoading(false);
+            }
+        }
+
+        fetchProducts();
+    }, []);
 
     const handlePageChange = (selectedPage) => {
         setCurrentPage(selectedPage.selected);
@@ -20,7 +34,6 @@ const ListProduct = () => {
     const displayedProducts = products.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
 
     const handleEdit = (productId) => {
-        // Sử dụng navigate thay vì Link
         navigate(`/editproduct/${productId}`);
     };
 
@@ -29,9 +42,12 @@ const ListProduct = () => {
     };
 
     const handleAddProduct = () => {
-        // Thêm logic để thực hiện hành động khi nút "Add Product" được nhấn
         console.log('Add Product button clicked');
     };
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div className="product-list-container-all">
@@ -50,6 +66,7 @@ const ListProduct = () => {
                             <div className="product-info">
                                 <span className="product-name">{product.name}</span>
                                 <span className="product-quantity">{`Quantity: ${product.quantity}`}</span>
+                                <span className="product-price">{`Price: $${product.price.toFixed(2)}`}</span>
                             </div>
                             <div className="product-buttons">
                                 <button className="edit" onClick={() => handleEdit(product.id)}>
